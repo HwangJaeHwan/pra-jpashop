e0eb881750361cc74ff235aec1af7a6cb7bec35b
HwangJaeHwan <zxcv0069@naver.com>
2021-05-21 19:26:48 +0900

    값 매핑

--- src/main/java/jpabook/jpashop/domain/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/domain/Delivery.java
@@ -12,10 +12,51 @@ public class Delivery extends BaseEntity {
     @Id
     @GeneratedValue
     private Long id;
-    private String city;
-    private String zipcode;
+    private String name;
+    @Embedded
+    private Address address;
     private DeliveryStatus status;
 
     @OneToOne(mappedBy = "delivery",fetch = LAZY)
     private Order order;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public DeliveryStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(DeliveryStatus status) {
+        this.status = status;
+    }
 }
